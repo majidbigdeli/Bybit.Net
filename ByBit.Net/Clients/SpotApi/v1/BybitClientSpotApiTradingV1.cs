479a02494ae4d5362a3559c1c01e367ff4f4449a
m.bigdeli <m.bigdeli@DEV-102>
2023-04-19 22:44:34 +0330

    .

--- ByBit.Net/Clients/SpotApi/v1/BybitClientSpotApiTradingV1.cs
+++ ByBit.Net/Clients/SpotApi/v1/BybitClientSpotApiTradingV1.cs
@@ -145,7 +145,7 @@ namespace Bybit.Net.Clients.SpotApi.v1
         #region Get user trades
 
         /// <inheritdoc />
-        public async Task<WebCallResult<IEnumerable<BybitSpotUserTrade>>> GetUserTradesAsync(string? symbol = null, DateTime? startTime = null, DateTime? endTime = null, long? fromId = null, long? toId = null, int? limit = null, long? receiveWindow = null, CancellationToken ct = default)
+        public async Task<WebCallResult<IEnumerable<BybitSpotUserTradeV1>>> GetUserTradesAsync(string? symbol = null, DateTime? startTime = null, DateTime? endTime = null, long? fromId = null, long? toId = null, int? limit = null, long? receiveWindow = null, CancellationToken ct = default)
         {
             var parameters = new Dictionary<string, object>();
             parameters.AddOptionalParameter("fromId", toId);
